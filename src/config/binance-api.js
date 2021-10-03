92361815c4c1f30529a5cf7ac8256b12844b7e5f
it18140330 <it18140330@my.sliit.lk>
2021-10-03 11:59:42 +0530

    binance API and select wallet files changed

--- src/config/binance-api.js
+++ src/config/binance-api.js
@@ -3,8 +3,19 @@ const ccxt = require('ccxt')
 async function getBinanceWalletFromAPI(apiKey, secret) {
     try {
         let binance = new ccxt.binance({ apiKey, secret })
-        const balance = await binance.fetchBalance();
-        return [null, balance];
+        const results = await binance.fetchBalance();
+        let balances = (results?.info?.balances) ? results.info.balances : [];
+        balances = balances.reduce((acc, current) => {
+            if (current.free > 0) {
+                acc.push({
+                    value: Number(current.free),
+                    symbol: current.asset
+                })
+            }
+            return acc;
+        }, [])
+
+        return [null, balances];
     } catch (err) {
         return [err, null]
     }
